a3cfda5d0e94298470e49bb2e4c26eb34953513b
shan-ti <thoselovelywords@gmail.com>
2025-08-23 08:02:55 +0900

    done with the general setup

--- src/components/feedback/FeedbackList.tsx
+++ src/components/feedback/FeedbackList.tsx
@@ -1,12 +1,28 @@
-
+import { TriangleUpIcon } from "@radix-ui/react-icons";
 
 const FeedbackList = () => {
   return (
     <ol className="feedback-list">
-      
+      <li className="feedback">
+        <button>
+          <TriangleUpIcon />
+          <span>593</span>
+        </button>
+
+        <div>B</div>
+
+        <div>
+          <p>ByteGrad</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Perferendis, voluptatem molestias? Beatae architecto aliquid soluta.
+          </p>
+        </div>
+
+        <p>4D</p>
+      </li>
     </ol>
-  )
-}
+  );
+};
 
-export default FeedbackList
-
+export default FeedbackList;
