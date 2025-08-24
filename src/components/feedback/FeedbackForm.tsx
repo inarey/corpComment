738ddadb5bf57703069acce836c3b1b82adcccb3
shan-ti <thoselovelywords@gmail.com>
2025-08-24 11:35:43 +0900

    setup feedback text length

--- src/components/feedback/FeedbackForm.tsx
+++ src/components/feedback/FeedbackForm.tsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
+import { MaxChars } from "../../lib/constants";
+
 const FeedbackForm = () => {
+  const [text, setText] = useState("");
+
+  const charCount = MaxChars - text.length;
   return (
     <form className="form">
-      <textarea placeholder="type text here" id="feedback-textarea" />
+      <textarea
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="type text here"
+        id="feedback-textarea"
+        spellCheck={false}
+        maxLength={MaxChars}
+      />
       <label htmlFor="feedback-textarea">
         Enter your feedback here, remember to # your company
       </label>
 
       <div>
-        <p className="u-italic">150</p>
+        <p className="u-italic">{charCount}</p>
         <button>
           <span>Submit</span>
         </button>
